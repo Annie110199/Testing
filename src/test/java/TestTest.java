838671c1b20872903c61b94fecea329a28a664b1
Galak <annagalakanna@gmail.com>
2021-11-17 13:57:31 +0600

    test

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,44 @@
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import java.util.*;
+
+public class TestTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void test() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AnnieG\\IdeaProjects\\Testing\\src\\main\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().wait(10);
+        driver.get("https://playground.learnqa.ru/puzzle/triangle");
+        driver.manage().window().setSize(new Dimension(1536, 824));
+        {
+            List<WebElement> elements = driver.findElements(By.id("show_answ"));
+            assert(elements.size() > 0);
+        }
+        driver.findElement(By.id("show_answ")).click();
+        {
+            List<WebElement> elements = driver.findElements(By.id("hide_answ"));
+            assert(elements.size() > 0);
+        }
+        {
+            List<WebElement> elements = driver.findElements(By.linkText("Ссылка на ответы"));
+            assert(elements.size() > 0);
+        }
+    }
+}
